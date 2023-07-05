eed5b72688ae456232d549ecc85844663c49b1e4
Adi Gunawan <adi_gunawan@live.com>
2023-07-05 09:00:55 +0800

    WIP

---
+++ src/gitlab.rs
@@ -0,0 +1,67 @@
+use chrono::{DateTime, Utc};
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+pub struct GitlabUser {
+    username: String,
+    token: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Commit {
+    id: String,
+    short_id: String,
+    title: String,
+    author_name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct  Repository {
+    pub id: u32,
+    pub name: String,
+    pub description: Option<String>,
+    pub visibility: String,
+}
+
+// get user commit message
+impl GitlabUser {
+
+    pub fn new(token: String) -> GitlabUser {
+        
+        GitlabUser { username: "".to_string(), token: token }
+    }
+
+    pub fn set_user(&mut self, username: String) {
+        self.username = username
+    }
+
+    pub async fn get_commit(&self, repo_id: u32, date: DateTime<Utc>) -> Result<Vec<Commit>, Box<dyn std::error::Error>> {
+        let url = format!("https://gitlab.com/api/v4/projects/{}/repository/commits?since={}", repo_id, date);
+        let client = reqwest::Client::new();
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("Bearer {}", self.token))?);
+
+        let response = client.get(&url).headers(headers).send().await?;
+        let commits = response.json::<Vec<Commit>>().await?;
+
+        Ok(commits)
+        
+    }
+
+    pub async fn get_repositories(&self) -> Result<Vec<Repository>, Box<dyn std::error::Error>> {
+        let url = "https://gitlab.com/api/v4/projects";
+        let client = reqwest::Client::new();
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("Bearer {}", self.token))?);
+
+        let response = client.get(url).headers(headers).send().await?;
+        let repositories = response.json::<Vec<Repository>>().await?;
+
+        Ok(repositories)
+    }
+
+    pub fn set_token(&mut self, token: String) {
+        self.token = token
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 use actix_web::{get, middleware, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
+use std::error::Error;
+use std::fmt::{self, Display};
+use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, env};
+use teloxide::{prelude::*, RequestError};
 use teloxide::types::Me;
-use std::{env};
-use teloxide::prelude::*;
+
+mod chatbot;
+mod gitlab;
+
+#[derive(Debug)]
+struct MyError {
+    message: String,
+}
+
+impl Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for MyError {}
+
+impl From<MyError> for RequestError {
+    fn from(error: MyError) -> Self {
+        RequestError::Api(teloxide::ApiError::BotBlocked)
+    }
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -10,13 +35,13 @@ async fn index() -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     dotenv().ok(); // Load the .env file if it exists
 
     // Retrieve the BOT_TOKEN from the environment variable
     let bot_token = env::var("BOT_TOKEN").expect("BOT_TOKEN not found in the environment");
-    let gitlab_access_token = env::var("GITLAB_ACCESS_TOKEN").expect("GITLAB_ACCESS_TOKEN not found in the environment");
-    
+    // store gitlab user data
+
     pretty_env_logger::init();
     log::info!("Starting digireport bot...");
 
@@ -40,19 +65,43 @@ async fn main() -> std::io::Result<()> {
     let u_me = me.unwrap();
 
     println!("Starting server...");
-    
+
     actix_rt::spawn({
+        let gitlab_users = Arc::new(Mutex::new(HashMap::<String, gitlab::GitlabUser>::new()));
+
         async move {
+            let cloned_map = gitlab_users.clone();
+
             println!("Bot info: {:?}", u_me.username());
             println!("Starting bot...");
-            teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-                bot.send_message(msg.chat.id, "text").await?;
+
+            chatbot::Command::repl(bot , move |msg: Message, bot: Bot, cmd: chatbot::Command| {
+                let cloned_map = cloned_map.clone(); // Clone the Arc
+                async move {
+                    let mut user = gitlab::GitlabUser::new("".to_string());
+                    if msg.chat.username().is_some() {
+                        
+                        let mut map = cloned_map.lock().unwrap();
+                        let username = msg.chat.username().unwrap();
+                        
+                        println!("username: {}", username);
+                        map.entry(username.to_string())
+                            .or_insert(gitlab::GitlabUser::new("".to_string()));
+
+                        user = map.get_mut(username).unwrap().clone();
+                    }
+
+                    // chatbot::answer(bot, msg, cmd, &mut user).await?;
+                    respond(());
+                }
+
                 respond(())
-            }).await;
+            })
+            .await;
         }
     });
 
-    HttpServer::new(move || {
+    match HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .service(index)
@@ -60,4 +109,15 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8077))?
     .run()
     .await
+    {
+        Ok(_) => {
+            println!("Server started");
+            Ok(())
+        }
+        Err(err) => {
+            println!("Error: {:?}", err);
+
+            Err(Box::new(err) as Box<dyn Error + Send + Sync>)
+        }
+    }
 }
